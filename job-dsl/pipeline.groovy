50cb43f16d0aa1f96e05510bad0378cfd208cb93
Jeff Zohrab <jzohrab@gmail.com>
2017-08-28 18:18:02 -0400

    WIP hack

--- job-dsl/pipeline.groovy
+++ job-dsl/pipeline.groovy
@@ -1,10 +1,11 @@
-pipelineJob('DSL Pipeline example 2') {
+pipelineJob('DSL_Pipeline_example_3') {
     triggers {
         scm('H/5 * * * *')
     }
     description('Pipeline for git://github.com/jzohrab/docker-demo.git')
     definition {
         cpsScm {
+	    sandbox()
             scm {
                 git('https://github.com/jzohrab/docker-demo.git') {
                    configure { it / extensions << '' }
